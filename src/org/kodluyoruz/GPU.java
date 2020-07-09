412f111a8a1bd5e832b1dd6472f7544ab3e10509
AbidinOzdurmaz <32870747+AbidinOzdurmaz@users.noreply.github.com>
2020-07-09 22:26:23 +0300

    Update GPU.java

--- src/org/kodluyoruz/GPU.java
+++ src/org/kodluyoruz/GPU.java
@@ -10,8 +10,10 @@ package org.kodluyoruz;
  *
  * TODO Bu 2 özellik ve bunların metotları için gereken kodları bu sınıfın içine yazın
  */
-public class GPU
+public class GPU extends Hardware
 {
+    private int memory;
+    private int bits;
 
     /*
      * Eğer ekran kartının hafızası 8 GB'tan fazlaysa, her 2 GB için fiyatı 250 TL artar.
@@ -19,4 +21,37 @@ public class GPU
      * TODO buna göre Hardware sınıfındaki fiyat hesaplayan metodu bu sınıfta yeniden yazın
      */
 
+
+    @Override
+    public void setPrice(double price) {
+        if(this.memory>8){
+            price += (((this.memory - 8) / 2) * 250);
+        }
+        if(this.bits>128){
+            price += (((this.bits - 128) / 64) * 400);
+        }
+        this.price=price;
+    }
+
+    @Override
+    public double getPrice() {
+        setPrice(this.price);
+        return this.price;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public int getBits() {
+        return bits;
+    }
+
+    public void setBits(int bits) {
+        this.bits = bits;
+    }
 }
